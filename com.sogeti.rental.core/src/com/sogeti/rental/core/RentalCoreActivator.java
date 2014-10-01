8bebe692909ad326fbe508821be1a97ae4a851d3
otoussai <otoussai@FR-31-02-13-152.corp.capgemini.com>
2014-10-01 09:23:37 +0200

    first commit

---
+++ com.sogeti.rental.core/src/com/sogeti/rental/core/RentalCoreActivator.java
@@ -0,0 +1,40 @@
+package com.sogeti.rental.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.helpers.RentalAgencyGenerator;
+
+public class RentalCoreActivator implements BundleActivator {
+
+
+	private static RentalAgency a=RentalAgencyGenerator.createSampleAgency();
+	
+	public static RentalAgency getAgency() {
+		return a;
+	}
+	
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		RentalCoreActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		RentalCoreActivator.context = null;
+	}
+
+}
